be9950fbdbb12457bc0980b69072904522466beb
Brendan Sapience <108418834+brendan-tg@users.noreply.github.com>
2023-11-02 15:50:00 -0700

    clean up

--- logics/MappingsLogics.py
+++ logics/MappingsLogics.py
@@ -4,6 +4,7 @@ import sys
 import os
 import urllib.parse
 import pandas as pd
+import logging
 
 sys.path.insert(1, './libs')
 sys.path.insert(1, './transformers')
@@ -88,20 +89,19 @@ def get_user_groups_resources(token,JsonData):
 # user query on users with filter -> get user ID and Group IDs
 # for each Group ID, get the list of Resource definitions
 # return the resource definitions 
-
-# TO DOs
-# account for multi page response in Resource list returned
-# move formatting code to transformers
 def get_user_mappings(outputFormat,sessionname,emailaddr):
     j = StdAPIUtils.generic_api_call_handler(sessionname,get_user_groups_resources,{'email':emailaddr})
-    
+    logging.debug("output of groups assigned to user: "+str(j))
     groupIDs = []
     alledges = j['data']['users']['edges']
+    
     if len(alledges) == 1:
         groups = alledges[0]['node']['groups']['edges']
         if len(groups) == 0:
             print("no group assigned to user.")
+            
         else:
+            logging.debug("groups assigned to user: "+str(groups))
             for grp in groups:
                 id = grp['node']['id']
                 groupIDs.append(id)
@@ -109,19 +109,21 @@ def get_user_mappings(outputFormat,sessionname,emailaddr):
     #    print("no corresponding user found.")
         
     allResources = []
-    hasMorePages = True
-    Cursor = "0"
+    
     for id in groupIDs:
-
+        hasMorePages = True
+        Cursor = "0"
+        logging.debug("retrieving resources for ID: "+str(id))
         while hasMorePages:
             a = StdAPIUtils.generic_api_call_handler(sessionname,get_resources_from_group_resources,{'cursor':Cursor,'id':id})
             pInfo = a['data']['group']['resources']['pageInfo']
+            logging.debug("pagination info: "+str(pInfo))
             hasMorePages = pInfo['hasNextPage']
             if hasMorePages:
                 Cursor = pInfo['endCursor']
 
             allresedges = a['data']['group']['resources']['edges']
-
+            logging.debug("resources retrieved: "+str(allresedges))
             if len(allresedges) != 0:
                 for res in allresedges:
                     aResource = res['node']
